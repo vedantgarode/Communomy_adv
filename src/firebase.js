dfc86b52c94486197bbb30f3b939491fd69b4fa4
Spartex <vedantgarode11@gmail.com>
2023-04-21 13:36:51 +0530

    test

--- src/firebase.js
+++ src/firebase.js
@@ -207,7 +207,7 @@ export const transact = async (user1, user2, amount, coin) => {
     return "Metamask Not Installed";
   }
 
-  const chainId = 80001;
+  const chainId = 11155111;
 
   if (window.ethereum.networkVersion !== chainId) {
     try {
@@ -221,10 +221,10 @@ export const transact = async (user1, user2, amount, coin) => {
           method: "wallet_addEthereumChain",
           params: [
             {
-              chainName: "Mumbai",
+              chainName: "Sepolia Test Network",
               chainId: "0x" + chainId.toString(16),
-              nativeCurrency: { name: "SEP", decimals: 18, symbol: "MATIC" },
-              rpcUrls: ["https://matic-mumbai.chainstacklabs.com"],
+              nativeCurrency: { name: "SEP", decimals: 18, symbol: "SEP" },
+              rpcUrls: ["https://rpc.sepolia.dev"],
             },
           ],
         });
